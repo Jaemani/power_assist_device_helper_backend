81a2b638a5b832ec258175d5fc484b1e1c9e4685
Jaemani <leejaeman0227@gmail.com>
2025-04-29 12:16:54 +0900

    changed to use cookie to store decodedQR since header doesn't support req-res wide. fixed naming issues

--- app/api/users/[userId]/route.js
+++ app/api/users/[userId]/route.js
@@ -1,10 +1,13 @@
 import { NextRequest } from 'next/server';
  
 export async function GET(req) {
-    const decodedUserId = req.headers.get('x-decoded-user-id');
+    const decodedUserId = req.cookies.get('decodedUserId')?.value;
 
     if (!decodedUserId) {
-        return new Response(JSON.stringify({ error: 'Decoded ID not found' }), { status: 400 });
+        return new Response(JSON.stringify({ error: 'Decoded ID not found' }), {
+      status: 400,
+      headers: { 'Content-Type': 'application/json' },
+    });
     }
 
     // e.g. Query a database for user with ID `userId`
--- middleware.js
+++ middleware.js
@@ -1,20 +1,20 @@
-import { decodeUserId } from '@/lib/middleware/decodeUserId';
-import { verifyAuth } from '@/lib/middleware/verifyAuth';
+import { decodeQR } from '@/lib/middleware/decryptQR';
+// import { verifyAuth } from '@/lib/middleware/verifyAuth';
 import { NextResponse } from 'next/server';
 
 export function middleware(req) {
   // Decode user ID
-  const decodeResponse = decodeUserId(req);
+  const decodeResponse = decodeQR(req);
   if (decodeResponse instanceof NextResponse && decodeResponse.status !== 200) {
     return decodeResponse;
   } // If object is an instance of ClassName, returns true.
 
   // Verify authentication
-  const authResponse = verifyAuth(req);
-  if (authResponse instanceof NextResponse && authResponse.status !== 200) {
-    return authResponse;
-  }
-  
+  // const authResponse = verifyAuth(req);
+  // if (authResponse instanceof NextResponse && authResponse.status !== 200) {
+  //   return authResponse;
+  // }
+
 
   // All middlewares passed
   return NextResponse.next();
