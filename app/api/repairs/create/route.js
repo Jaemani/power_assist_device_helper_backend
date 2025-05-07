0993cc9146e10fe2cefb0f886163f45019f45a51
Jaemani <leejaeman0227@gmail.com>
2025-05-08 05:08:10 +0900

    수리이력 생셩시 SMS 발신기능 추가

--- app/api/repairs/create/route.js
+++ app/api/repairs/create/route.js
@@ -1,6 +1,5 @@
-// 새로운 수리 이력을 생성하는 API 
-
 import { NextResponse } from 'next/server';
+import { createMessage } from '@/lib/sms';
 
 export async function POST(request) {
   try {
@@ -8,17 +7,21 @@ export async function POST(request) {
 
     console.info(`[Repair API] 수리 이력 생성 요청 수신`, data);
 
-    if (!data.description || !data.date) {
-      return NextResponse.json(
-        { success: false, error: '수리 설명과 날짜를 입력해야 합니다.' },
-        { status: 400 }
-      );
-    }
+    // if (!data.description || !data.date) {
+    //   return NextResponse.json(
+    //     { success: false, error: '수리 설명과 날짜를 입력해야 합니다.' },
+    //     { status: 400 }
+    //   );
+    // }
+    
+    const userPhoneNumber = "01000000000";
+    createMessage("수리수리 마수리 -  수리이력 생성되었습니다", userPhoneNumber);
 
     return NextResponse.json(
       { success: true, message: '수리 이력이 성공적으로 생성되었습니다.' },
       { status: 201 }
     );
+
   } catch (error) {
     console.error(`[Repair API][Error] 수리 이력 생성 실패: ${error.message}`);
 
