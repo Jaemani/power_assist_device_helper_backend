0221627cea8212299326ca379db4730457f16e92
Jaemani <leejaeman0227@gmail.com>
2025-05-08 03:37:04 +0900

    user, guardian의 로그인, 회원가입 구현

--- app/api/auth/login/route.js
+++ app/api/auth/login/route.js
@@ -1,5 +1,5 @@
 import { getAuth } from 'firebase-admin/auth';
-import { getUsersCollection } from '@/lib/db/models';
+import { getUsersCollection, getGuardiansCollection } from '@/lib/db/models';
 import { signToken } from '@/lib/auth/JWT';
 
 export async function POST(req) {
@@ -9,7 +9,7 @@ export async function POST(req) {
   const decoded = await getAuth().verifyIdToken(idToken);
   const firebaseUid = decoded.uid;
 
-  // 사용자 . Mongodb와 연결
+  const guardians = await getGuardiansCollection();
   const users = await getUsersCollection();
   const user = await users.findOne({ firebaseUid });
 
@@ -17,9 +17,18 @@ export async function POST(req) {
     return new Response(JSON.stringify({ status: 'register', firebaseUid }), { status: 200 });
   }
 
+  let guardian_uid = "not guardian"; // default
+
+  if (user.role === 'guardian') { // 보호자일 경우 보호대상 유저의 id 받아옴
+    const guardian = await guardians.findOne({ userId: user._id });
+    guardian_uid = guardian.userId.toString();
+  }
+
+
+
   const token = signToken({ uid: user._id.toString(), role: user.role });
 
-  return new Response(JSON.stringify({ status: 'login', token }), {
+  return new Response(JSON.stringify({ status: 'login', token, guardian_uid }), {
     status: 200,
     headers: { 'Content-Type': 'application/json' },
   });
--- app/api/auth/register/route.js
+++ app/api/auth/register/route.js
@@ -1,15 +1,83 @@
-import { NextRequest } from 'next/server';
-import { getUsersCollection } from '@/lib/db/models';
+import { getAuth } from 'firebase-admin/auth';
+import { getUsersCollection, getGuardiansCollection } from '@/lib/db/models';
 
 export async function POST(req) {
-    const body = await req.json();
+    const { idToken, phoneNumber, role, vehicleId, userPhoneNumber } = await req.json(); // userPhoneNumber: 보호자일 경우, 보호대상 유저의 전화번호
+    if (!idToken) return new Response('Missing firebase idToken', { status: 400 });
 
-    const { uid, nickname, vehicleId } = body;
+    const decoded = await getAuth().verifyIdToken(idToken);
+    const firebaseUid = decoded.uid;
 
-    return new Response(JSON.stringify({ 
-        message: `new User Registered!`, 
-    }), {
-        status: 200,
-        headers: { 'Content-Type': 'application/json' },
-    });
+    const users = await getUsersCollection();
+    const guardians = await getGuardiansCollection();
+
+    try{
+        const user = await users.findOne({ firebaseUid });
+        if (user) {
+            return new Response(JSON.stringify({ error: 'User already exists' }), {
+                status: 400,
+                headers: { 'Content-Type': 'application/json' },
+            });
+        }
+    
+        if(role === 'user') { // 유저는 가입시 차량 QR코드 스캔하여 얻은 vehicleId를 입력
+            await users.insertOne({
+                firebaseUid,
+                phoneNumber,
+                role,
+                vehicleIds: [vehicleId], // array of ObjectId
+                guardianIds: [], // array of ObjectId
+                createDate: new Date(),
+                updateDate: new Date(),
+            });
+        }
+        else if (role === 'guardian') { // 보호자는 가입시 유저 전화번호를 입력
+            await users.insertOne({
+                firebaseUid,
+                phoneNumber,
+                role,
+                vehicleIds: [], // empty for guardian
+                guardianIds: [], // empty for guardian
+                createDate: new Date(),
+                updateDate: new Date(),
+            });
+
+            // 보호자 - 보호대상자 등록
+            const gUser = await users.findOne({ phoneNumber: userPhoneNumber });
+            if (!gUser) {
+                return new Response(JSON.stringify({ error: 'Target User not found (guardian)' }), {
+                    status: 400,
+                    headers: { 'Content-Type': 'application/json' },
+                });
+            }
+
+            const addedGuardian = await guardians.insertOne({
+                firebaseUid: firebaseUid,
+                userId: gUser._id,
+                createDate: new Date(),
+                updateDate: new Date(),
+            });
+
+            await users.updateOne( // 보호대상 유저의 보호자 리스트에 보호자 ObjectId 추가
+                { _id: gUser._id },
+                { $addToSet: { guardianIds: addedGuardian.insertedId } }
+            );
+        }
+
+        return new Response(JSON.stringify({ 
+            message: `new User Registered!`, 
+        }), {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+    }catch (error) {
+        console.error('Error creating user:', error);
+        return new Response(JSON.stringify({ error: 'Error creating user' }), {
+            status: 500,
+            headers: { 'Content-Type': 'application/json' },
+        });
+    }
+
+    
 }
