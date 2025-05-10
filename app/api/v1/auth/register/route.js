f071dea33028e9892aa5d7271e22f0af20a98247
Jaemani <leejaeman0227@gmail.com>
2025-05-11 00:27:27 +0900

    db 연결과 동시에 firebase initialization 진행.

--- app/api/v1/auth/register/route.js
+++ app/api/v1/auth/register/route.js
@@ -1,9 +1,12 @@
 import { NextResponse } from 'next/server';
 import { connectToMongoose } from '@/lib/db/connect';
+import { initializeFirebaseAdmin} from '@/lib/firebaseAdmin';
 // import { User } from '@/lib/db/models';
 import { getAuth } from 'firebase-admin/auth';
 
+// await connectToMongoose();
 await connectToMongoose();
+await initializeFirebaseAdmin();
 
 export async function POST(req) {
 
