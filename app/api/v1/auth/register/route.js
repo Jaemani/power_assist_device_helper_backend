1ec07948e7811bdf2f95503d12af46dc3ddeb41d
Jaemani <leejaeman0227@gmail.com>
2025-05-11 00:27:22 +0900

    db 연결과 동시에 firebase initialization 진행.

--- app/api/v1/auth/register/route.js
+++ app/api/v1/auth/register/route.js
@@ -1,37 +1,46 @@
 import { NextResponse } from 'next/server';
 import { connectToMongoose } from '@/lib/db/connect';
-import User from '@/lib/db/models';
+// import { User } from '@/lib/db/models';
 import { getAuth } from 'firebase-admin/auth';
 
 await connectToMongoose();
 
 export async function POST(req) {
+
     const { idToken, phoneNumber, role, vehicleId } = await req.json();
     if (!idToken) return NextResponse.json({ error: 'Missing firebase idToken' }, { status: 400 });
 
-    const decoded = await getAuth().verifyIdToken(idToken);
-    console.log('Decoded token:', decoded);
-    const firebaseUid = decoded.uid;
-
-
     try {
-        const user = await User.findOne({ firebaseUid });
-        if (user) {
-            return NextResponse.json({ error: 'User already exists' }, { status: 400 });
-        }
+        const decoded = await getAuth().verifyIdToken(idToken);
+        console.log('Decoded token:', decoded);
+        const firebaseUid = decoded.uid;
 
-        await User.insertOne({
-            firebaseUid,
-            phoneNumber,
-            role,
-            vehicleIds: [vehicleId], // array of ObjectId
-            guardianIds: [], // array of ObjectId
-        });
-
-        return NextResponse.json({ message: 'new User Registered!' }, { status: 200 });
+        return NextResponse.json({ message: 'User Registered!' }, { status: 200 });
 
     } catch (error) {
-        console.error('Error creating user:', error);
-        return NextResponse.json({ error: 'Error creating user' }, { status: 500 });
+        console.error('Error verifying ID token:', error);
+        return NextResponse.json({ error: 'Invalid ID token' }, { status: 401 });
     }
+
+
+    // try {
+    //     const user = await User.findOne({ firebaseUid });
+    //     if (user) {
+    //         return NextResponse.json({ error: 'User already exists' }, { status: 400 });
+    //     }
+
+    //     await User.insertOne({
+    //         firebaseUid,
+    //         phoneNumber,
+    //         role,
+    //         vehicleIds: [vehicleId], // array of ObjectId
+    //         guardianIds: [], // array of ObjectId
+    //     });
+
+    //     return NextResponse.json({ message: 'new User Registered!' }, { status: 200 });
+
+    // } catch (error) {
+    //     console.error('Error creating user:', error);
+    //     return NextResponse.json({ error: 'Error creating user' }, { status: 500 });
+    // }
 }
