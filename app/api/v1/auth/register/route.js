f51d106a85130009ecce7862668100731ec5e2ef
Jaemani <leejaeman0227@gmail.com>
2025-05-11 01:55:39 +0900

    firebase initialized, register 구현

--- app/api/v1/auth/register/route.js
+++ app/api/v1/auth/register/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { connectToMongoose } from '@/lib/db/connect';
-import { initializeFirebaseAdmin} from '@/lib/firebaseAdmin';
-// import { User } from '@/lib/db/models'; 
+import initializeFirebaseAdmin from '@/lib/firebaseAdmin';
+import { User } from '@/lib/db/models'; 
 import { getAuth } from 'firebase-admin/auth';
 
 // await connectToMongoose();
@@ -9,41 +9,48 @@ await connectToMongoose();
 await initializeFirebaseAdmin();
 
 export async function POST(req) {
-
-    const { idToken, phoneNumber, role, vehicleId } = await req.json();
-    if (!idToken) return NextResponse.json({ error: 'Missing firebase idToken' }, { status: 400 });
-
     try {
-        const decoded = await getAuth().verifyIdToken(idToken);
-        console.log('Decoded token:', decoded);
-        const firebaseUid = decoded.uid;
+        const { idToken, phoneNumber, role, vehicleId } = await req.json();
+        if (!idToken) return NextResponse.json({ error: 'Missing firebase idToken' }, { status: 400 });
 
-        return NextResponse.json({ message: 'User Registered!' }, { status: 200 });
+        let firebaseUid;
+        try {
+            const decoded = await getAuth().verifyIdToken(idToken);
+            console.log('Decoded token:', decoded);
+            firebaseUid = decoded.uid;
 
-    } catch (error) {
-        console.error('Error verifying ID token:', error);
-        return NextResponse.json({ error: 'Invalid ID token' }, { status: 401 });
+        } catch (error) {
+            console.error('Error verifying ID token:', error);
+            return NextResponse.json({ error: 'Invalid ID token' }, { status: 401 });
+        } 
+
+
+        try {
+            if (firebaseUid === undefined) {
+                return NextResponse.json({ error: 'Invalid ID token' }, { status: 401 });
+            }
+            
+            const user = await User.findOne({ firebaseUid });
+            if (user) {
+                return NextResponse.json({ error: 'User already exists' }, { status: 400 });
+            }
+
+            await User.insertOne({
+                firebaseUid,
+                phoneNumber,
+                role,
+                guardianIds: [], // array of ObjectId
+            });
+
+            return NextResponse.json({ message: 'new User Registered!' }, { status: 200 });
+
+        } catch (error) {
+            console.error('Error creating user:', error);
+            return NextResponse.json({ error: 'Error creating user' }, { status: 500 });
+        }
+
+    }catch (error) {
+        console.error('Error in POST function:', error);
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
     }
-
-
-    // try {
-    //     const user = await User.findOne({ firebaseUid });
-    //     if (user) {
-    //         return NextResponse.json({ error: 'User already exists' }, { status: 400 });
-    //     }
-
-    //     await User.insertOne({
-    //         firebaseUid,
-    //         phoneNumber,
-    //         role,
-    //         vehicleIds: [vehicleId], // array of ObjectId
-    //         guardianIds: [], // array of ObjectId
-    //     });
-
-    //     return NextResponse.json({ message: 'new User Registered!' }, { status: 200 });
-
-    // } catch (error) {
-    //     console.error('Error creating user:', error);
-    //     return NextResponse.json({ error: 'Error creating user' }, { status: 500 });
-    // }
 }
