f68b0b7f6f30cea50a5af5c8bd2b6158f9193c34
Jaemani <leejaeman0227@gmail.com>
2025-05-13 11:55:51 +0900

    repairstations

--- app/api/v1/repairStations/route.js
+++ app/api/v1/repairStations/route.js
@@ -1,20 +1,21 @@
 import { NextResponse } from 'next/server';
 import connectToMongoose from '@/lib/db/connect';
-import { RepairStationList } from '@/lib/db/models'; 
-import { withAuth } from '@/lib/auth/withAuth';
+import { RepairStations } from '@/lib/db/models'; 
+// import { withAuth } from '@/lib/auth/withAuth';
 
 await connectToMongoose();
 
-export const GET = withAuth(async (req, ctx, decoded) => {
+export const GET = async (req) => {
     try {
-        const stations = await RepairStationList.find().lean();
+        const stations = await RepairStations.find().lean();
 
         const simplified = stations.map((station) => ({
+            code: station.code,
             state: station.state,
             city: station.city,
             region: station.region,
             address: station.address,
-            name: station.name,
+            label: station.name,
             telephone: station.telephone,
             coordinate: station.coordinate.coordinates, // [lng, lat] 만 전달
         }));
@@ -27,4 +28,4 @@ export const GET = withAuth(async (req, ctx, decoded) => {
         console.error('Error in GET function:', error);
         return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
     }
-});
+}
