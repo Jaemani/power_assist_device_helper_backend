ca44a9f69b1cc5c1439a17eb546865d039cecafd
Jaemani <leejaeman0227@gmail.com>
2025-05-22 02:34:34 +0900

    SMS send bug

--- app/api/v1/vehicles/[vehicleId]/self-check/route.js
+++ app/api/v1/vehicles/[vehicleId]/self-check/route.js
@@ -61,6 +61,27 @@ export const POST = withAuth(async (req, { params }, decoded) => {
     );
   }
 
+  console.log("자가점검 기록 저장 요청", {
+    vehicleId,
+    userId: user._id,
+    motorNoise, 
+    abnormalSpeed, 
+    batteryBlinking, 
+    chargingNotStart, 
+    breakDelay, 
+    breakPadIssue, 
+    tubePunctureFrequent, 
+    tireWearFrequent, 
+    batteryDischargeFast, 
+    incompleteCharging, 
+    seatUnstable, 
+    seatCoverIssue, 
+    footRestLoose, 
+    antislipWorn, 
+    frameNoise, 
+    frameCrack,
+  });
+
   // — DB에 자가점검 기록 저장
   await SelfChecks.create({
     vehicleId: new mongoose.Types.ObjectId(vehicle._id.toString()),
@@ -84,29 +105,27 @@ export const POST = withAuth(async (req, { params }, decoded) => {
     checkedAt: Date.now(),
   });
 
-  const detail = motorNoise? "구동 시 모터에서 시끄러운 소음이나 심한 진동\n" : ""
-    + abnormalSpeed? "속도가 너무 느리거나 너무 빠르게 느껴짐\n" : ""
-    + batteryBlinking? "계기판에 배터리 경고등 점멸\n" : ""
-    + chargingNotStart? "충전이 안됨\n" : ""
-    + breakDelay? "브레이크 작동이 지연됨\n" : ""
-    + breakPadIssue? "브레이크 패드 마모 또는 금이 감\n" : ""
-    + tubePunctureFrequent? "타이어 펑크가 잦음\n" : ""
-    + tireWearFrequent? "타이어 마모가 잦음\n" : ""
-    + batteryDischargeFast? "배터리 방전이 잦음\n" : ""
-    + incompleteCharging? "완충이 안됨\n" : ""
-    + seatUnstable? "시트가 느슨함\n" : ""
-    + seatCoverIssue? "시트 커버 손상\n" : "" 
-    + footRestLoose? "발걸이가 느슨함\n" : ""
-    + antislipWorn? "미끄럼 방지 고무 패드 마모\n" : ""
-    + frameNoise? "프레임에서 소음 발생\n" : ""
-    + frameCrack? "프레임이 깨지거나 금이 가거나 휘어짐\n" : "";
+  const detail =
+    (motorNoise? "구동 시 모터에서 시끄러운 소음이나 심한 진동\n" : "") +
+    (abnormalSpeed? "속도가 너무 느리거나 너무 빠르게 느껴짐\n" : "") +
+    (batteryBlinking? "계기판에 배터리 경고등 점멸\n" : "") +
+    (chargingNotStart? "충전이 안됨\n" : "") +
+    (breakDelay? "브레이크 작동이 지연됨\n" : "") +
+    (breakPadIssue? "브레이크 패드 마모 또는 금이 감\n" : "") +
+    (tubePunctureFrequent? "타이어 펑크가 잦음\n" : "") +
+    (tireWearFrequent? "타이어 마모가 잦음\n" : "") +
+    (batteryDischargeFast? "배터리 방전이 잦음\n" : "") +
+    (incompleteCharging? "완충이 안됨\n" : "") +
+    (seatUnstable? "시트가 느슨함\n" : "") +
+    (seatCoverIssue? "시트 커버 손상\n" : "" ) +
+    (footRestLoose? "발걸이가 느슨함\n" : "") +
+    (antislipWorn? "미끄럼 방지 고무 패드 마모\n" : "") +
+    (frameNoise? "프레임에서 소음 발생\n" : "") +
+    (frameCrack? "프레임이 깨지거나 금이 가거나 휘어짐\n" : "");
 
   // — 이상 감지 & 동의된 경우에만 SMS 전송
   if (user.smsConsent && detail.length > 0) {
-    const text = `⚠️ 자가점검 이상 알림
-        전동보장구ID: ${vehicleId}
-        사용자: ${user.name}
-        상세: ${detail}`;
+    const text = `* 자가점검 이상 알림\n- 전동보장구ID: ${vehicleId}\n- 사용자: ${user.name}\n- 상세: \n${detail}`;
     await sendSms(text, process.env.MANAGER_PHONE);
   }
 
